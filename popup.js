fb07496ed6416c2001cb3fcd94e83a245320108b
Alejandro Diaz <99855551+Dev-Alejo@users.noreply.github.com>
2022-04-11 22:03:40 -0400

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,12 @@
+
+let overlay = document.querySelector('#overlay');
+// let popup = getElementById('pop-up');
+
+// let btnCerrarpPopup = getElementById('btn-cerrar-popup');
+
+let cerrarPopup = document.querySelector("#btn-cerrar-popup");
+
+cerrarPopup.addEventListener('click',function(){
+    overlay.style.display= "none";
+
+})
